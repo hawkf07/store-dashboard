d5130d89c671d8cf86b1b6c8c044d16a991fdf8f
hawkf07 <hawkf07@proton.me>
2023-05-02 11:24:57 +0700

    make sidebar more responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 export const Sidebar = () => {
   return (
-    <aside className="left-0 top-0 h-screen w-96 bg-gray-200 md:w-64 md:flex-none "></aside>
+    <aside className=" h-screen w-96 bg-gray-50 shadow md:w-64 md:flex-none "></aside>
   );
 };
